3ae5521c145b238e2c0e92d9a5eb0146a58a553c
Yaroslav82 <yargay230607@gmail.com>
2023-07-25 22:52:11 +0300

    Add finding method

--- src/tasks/Task_02.java
+++ src/tasks/Task_02.java
@@ -7,6 +7,14 @@ public class Task_02 {
 
     private final static Map<String, Integer> products = getProducts();
 
+    private static void findProduct(String product) {
+        if (products.containsKey(product)) {
+            System.out.println("Product name: " + product + "\nQuantity: " + products.get(product));
+        } else {
+            System.out.println("We couldn't find this product :(");
+        }
+    }
+
     private static Map<String, Integer> getProducts() {
         return new HashMap<>(Map.of("pencil", 42, "notebook", 32, "book", 20, "pen", 50, "ruler", 10));
     }
