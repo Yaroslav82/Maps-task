5dd7c7247e42b754998d676a88bed14a1771c161
Yaroslav82 <yargay230607@gmail.com>
2023-07-25 22:33:32 +0300

    Add main method

--- src/tasks/Task_01.java
+++ src/tasks/Task_01.java
@@ -6,6 +6,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Task_01 {
 
+    public static void main(String[] args) {
+        Map<String, Integer> products = getProducts();
+        getOutput("Initial data:", products);
+        updateProducts(products);
+        getOutput("Updated data:", products);
+    }
+
     private static void getOutput(String title, Map<String, Integer> products) {
         System.out.println(title);
         AtomicInteger counter = new AtomicInteger(1);
