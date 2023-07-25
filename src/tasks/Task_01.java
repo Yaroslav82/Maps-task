d188a82183cb3cd6a5a165df2cb6c08df873f0f7
Yaroslav82 <yargay230607@gmail.com>
2023-07-25 22:24:37 +0300

    Add product updating

--- src/tasks/Task_01.java
+++ src/tasks/Task_01.java
@@ -1,10 +1,16 @@
 package tasks;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Task_01 {
 
+    private static void updateProducts(Map<String, Integer> products) {
+        products.put("banana", 14);
+        products.put("plum", 15);
+    }
+
     private static Map<String, Integer> getProducts() {
-        return Map.of("orange", 12, "banana", 25, "lemon", 8, "pineapple", 13, "grape", 9);
+        return new HashMap<>(Map.of("orange", 12, "banana", 25, "lemon", 8, "pineapple", 13, "grape", 9));
     }
 }
