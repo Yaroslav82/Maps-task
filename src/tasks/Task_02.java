5531e09bbd7b5a86f444f7279daaf45f6583f26c
Yaroslav82 <yargay230607@gmail.com>
2023-07-25 22:51:34 +0300

    Create products constant

--- src/tasks/Task_02.java
+++ src/tasks/Task_02.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 public class Task_02 {
 
+    private final static Map<String, Integer> products = getProducts();
+
     private static Map<String, Integer> getProducts() {
         return new HashMap<>(Map.of("pencil", 42, "notebook", 32, "book", 20, "pen", 50, "ruler", 10));
     }
