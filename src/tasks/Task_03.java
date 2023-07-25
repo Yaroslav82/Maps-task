4a4fa1a20c18859abc21c4e1cd39519a6e4e4700
Yaroslav82 <yargay230607@gmail.com>
2023-07-25 23:16:53 +0300

    Add contacts editor

--- src/tasks/Task_03.java
+++ src/tasks/Task_03.java
@@ -5,7 +5,9 @@ import java.util.Map;
 
 public class Task_03 {
 
-
+    private static void editContacts(Map<String, String> contacts) {
+        contacts.replace("Tom", "tomasdev@glob.net");
+    }
 
     private static Map<String, String> getContacts() {
         return new LinkedHashMap<>(Map.of("Tom", "tom@mail.com", "Lisa", "lisa@mail.com", "Alice", "alisa@mail.com", "Denis", "den@mail.com"));
