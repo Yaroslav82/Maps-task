318d9ef06c4a6df7e0885a2c08b1bb7e0b00d1f9
Yaroslav82 <yargay230607@gmail.com>
2023-07-25 22:15:27 +0300

    Add product map creator

---
+++ src/tasks/Task_01.java
@@ -0,0 +1,10 @@
+package tasks;
+
+import java.util.Map;
+
+public class Task_01 {
+
+    private static Map<String, Integer> getProducts() {
+        return Map.of("orange", 12, "banana", 25, "lemon", 8, "pineapple", 13, "grape", 9);
+    }
+}
